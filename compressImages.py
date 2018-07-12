f6e5c2caeb7d893ce3f26c57e23e4466c708638c
Ulises Castro <ulisescastro2015@gmail.com>
2018-07-12 16:40:40 -0500

    Check image and putting its location
    Checking if it is an image and it works on python 2 now.

--- compressImages.py
+++ compressImages.py
@@ -1,9 +1,12 @@
 import os
 from PIL import Image
 
-imagesNames = os.listdir()
-for imagename in imagesNames:
-    sizeImage = os.path.getsize(imagename)
-    if(sizeImage > 700000):
-        image = Image.open(imagename)
-        image.save(imagename, quality=20, optimize=True)
+validExtensions = ['jpeg','jpg','gif','png']
+imagesNames = os.listdir(os.path.dirname(os.path.realpath(__file__)))
+for imageName in imagesNames:
+    extension = imageName.split('.')
+    extension = extension[len(extension) - 1]
+    sizeImage = os.path.getsize(imageName)
+    if(sizeImage > 500000 and extension in validExtensions):
+        image = Image.open(imageName)
+        image.save(imageName, quality=15)
